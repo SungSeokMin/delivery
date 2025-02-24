685e2bd00f8ea8d529798b53ce06845c089274f7
sungseokmin <jkl154527@gmail.com>
2025-02-24 18:36:43 +0900

    User Microservice

---
+++ apps/user/src/auth/auth.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UseInterceptors,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register-dto';
+import { Authorization } from './decorator/authorization.decorator';
+
+@Controller('auth')
+@UseInterceptors(ClassSerializerInterceptor)
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  @UsePipes(ValidationPipe)
+  registerUser(
+    @Authorization() token: string,
+    @Body() registerDto: RegisterDto,
+  ) {
+    if (token === null) {
+      throw new UnauthorizedException('토큰을 입력해주세요.');
+    }
+
+    return this.authService.register(token, registerDto);
+  }
+
+  @Post('login')
+  @UsePipes(ValidationPipe)
+  loginUser(@Authorization() token: string) {
+    if (token === null) {
+      throw new UnauthorizedException('토큰을 입력해주세요.');
+    }
+
+    return this.authService.login(token);
+  }
+}
---
+++ apps/user/src/auth/auth.service.ts
@@ -0,0 +1,108 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { RegisterDto } from './dto/register-dto';
+import { UserService } from '../user/user.service';
+import { Authorization } from './decorator/authorization.decorator';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../user/entity/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  register(@Authorization() basicToken: string, registerDto: RegisterDto) {
+    const { email, password } = this.parseBasicToken(basicToken);
+
+    return this.userService.create({
+      ...registerDto,
+      email,
+      password,
+    });
+  }
+
+  async login(rawToken: string) {
+    const { email, password } = this.parseBasicToken(rawToken);
+
+    const user = await this.authenticate(email, password);
+
+    return {
+      accessToken: await this.issueToken(user, false),
+      refreshToken: await this.issueToken(user, true),
+    };
+  }
+
+  parseBasicToken(basicToken: string) {
+    const basicSplit = basicToken.split(' ');
+
+    if (basicSplit.length !== 2) {
+      throw new BadRequestException('토큰 포맷이 잘못됐습니다.');
+    }
+
+    const [basic, token] = basicSplit;
+
+    if (basic.toLowerCase() !== 'basic') {
+      throw new BadRequestException('토큰 포맷이 잘못됐습니다.');
+    }
+
+    const decoded = Buffer.from(token, 'base64').toString('utf-8');
+
+    const tokenSplit = decoded.split(':');
+
+    if (tokenSplit.length !== 2) {
+      throw new BadRequestException('토큰 포맷이 잘못됐습니다.');
+    }
+
+    const [email, password] = tokenSplit;
+
+    return { email, password };
+  }
+
+  async authenticate(email: string, password: string) {
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { id: true, email: true, password: true },
+    });
+
+    if (!user) {
+      throw new BadRequestException('잘못된 로그인 정보입니다.');
+    }
+
+    const passOk = await bcrypt.compare(password, user.password);
+
+    if (!passOk) {
+      throw new BadRequestException('잘못된 로그인 정보입니다.');
+    }
+
+    return user;
+  }
+
+  async issueToken(user: any, isRefreshToken: boolean) {
+    const accessTokenSecret = this.configService.getOrThrow<string>(
+      'ACCESS_TOKEN_SECRET',
+    );
+    const refreshTokenSecret = this.configService.getOrThrow<string>(
+      'REFRESH_TOKEN_SECRET',
+    );
+
+    return this.jwtService.signAsync(
+      {
+        sub: user.id ?? user.sub,
+        role: user.role,
+        type: isRefreshToken ? 'refresh' : 'access',
+      },
+      {
+        secret: isRefreshToken ? refreshTokenSecret : accessTokenSecret,
+        expiresIn: '3600h',
+      },
+    );
+  }
+}
